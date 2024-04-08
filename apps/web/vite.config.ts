a6f6800ac3ed04c01b682665af770a9ebcdd907a
aaort <artashesano@gmail.com>
2024-04-09 00:22:13 +0400

    Configure proxies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -16,8 +16,11 @@ export default defineConfig({
     }),
   ],
   server: {
-    cors: {
-      origin: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
     },
   },
   build: {
