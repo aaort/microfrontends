29c8f51c978a01f7cd892867fef88f5cc2adda0c
aaort <artashesano@gmail.com>
2024-04-09 11:46:38 +0400

    Improvements on cors issue

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,5 @@
 import federation from "@originjs/vite-plugin-federation";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -9,24 +10,12 @@ export default defineConfig({
     federation({
       name: "web",
       remotes: {
-        components: `await fetch('https://microfrontends-components-40n7usmsp-aaorts-projects.vercel.app/assets/remoteEntry.js', { headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers":
-              "X-Requested-With, content-type, Authorization",
-          },})`,
+        components:
+          "https://microfrontends-components.vercel.app/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom"],
     }),
   ],
-  server: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
-    },
-  },
   build: {
     modulePreload: false,
     target: "esnext",
