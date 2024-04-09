38c505405381483622c1ce8a1a001787132543da
aaort <artashesano@gmail.com>
2024-04-09 11:50:07 +0400

    Updates on button component

--- packages/components/src/components/Button.tsx
+++ packages/components/src/components/Button.tsx
@@ -5,7 +5,10 @@ function Button(props: ComponentProps<"button">) {
     <button
       style={{
         backgroundColor: "#404040",
-        borderRadius: "10px",
+        borderRadius: 0,
+        borderWidth: 1,
+        borderColor: "green",
+        borderStyle: "dashed",
         paddingInline: "15px",
         paddingBlock: "10px",
       }}
