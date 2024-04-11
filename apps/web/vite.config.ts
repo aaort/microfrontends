6bf76451b94b2b00e46679c32317f4f9511fb47c
aaort <artashesano@gmail.com>
2024-04-11 17:36:50 +0400

    Set proper remote components URL for different node evns

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,26 +1,33 @@
 import federation from "@originjs/vite-plugin-federation";
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "web",
-      remotes: {
-        components: "http://localhost:5001/assets/remoteEntry.js",
-        // components:
-        //   "https://microfrontends-components.vercel.app/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const componentsURL =
+    env.NODE_ENV === "production"
+      ? "https://microfrontends-components.vercel.app/assets/remoteEntry.js"
+      : "http://localhost:5001/assets/remoteEntry.js";
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "web",
+        remotes: {
+          components: componentsURL,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
