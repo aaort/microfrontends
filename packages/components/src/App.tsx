18ed0e4d796f43e11e548a695561b87da38c0892
aaort <artashesano@gmail.com>
2024-04-15 12:30:11 +0400

    Add link to remoteEntry in components home page

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>this app only exposes some useful remote components</h1>;
+  return (
+    <h1>
+      this app only exposes some useful remote components through{" "}
+      <a href="/assets/remoteEntry.js">remoteEntry</a>
+    </h1>
+  );
 }
 
 export default App;
